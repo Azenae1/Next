b79eb86c3311d7128214d704c0fac1a19faac929
Yury Bursian <dab180996@gmail.com>
2024-09-22 16:05:11 +0300

    build created

--- app/post/[id]/page.js
+++ app/post/[id]/page.js
@@ -10,17 +10,36 @@ export async function generateMetadata({ params, searchParams }) {
 
 async function fetchData(id) {
   const res = await fetch("https://jsonplaceholder.typicode.com/posts/" + id);
+  if (!res.ok) {
+    throw new Error(`Не удалось загрузить пост с id ${id}: ${res.statusText}`);
+  }
   const result = await res.json();
   return result;
 }
 
+export async function generateStaticParams() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  if (!res.ok) {
+    throw new Error(`Can't load posts: ${res.statusText}`);
+  }
+  const posts = await res.json();
+
+  return posts.map((post) => ({
+    id: post.id.toString(),
+  }));
+}
+
 const PagePost = async ({ params: { id } }) => {
-  const post = await fetchData(id);
-  return (
-    <div className="post">
-      <Post post={post} />
-    </div>
-  );
+  try {
+    const post = await fetchData(id);
+    return (
+      <div className="post">
+        <Post post={post} />
+      </div>
+    );
+  } catch (error) {
+    return <div>Error occured: {error.message}</div>;
+  }
 };
 
 export default PagePost;
